c8281ed78c2f82b438bf061a8010ca2c039c340d
Danztee <olowoniyidaniel@gmail.com>
2023-01-12 21:53:05 +0100

    update

--- basics.ts
+++ basics.ts
@@ -39,11 +39,12 @@ function printOut(value: any) {
 }
 
 // Generics
-function insertAtBeginning(array: any[], value: any) {
-  const newArray = [value, ...array];
-  return newArray;
-}
+// function insertAtBeginning<T>(array: T[], value: T) {
+//   const newArray = [value, ...array];
+//   return newArray;
+// }
 
-const demoArray = [1, 2, 3];
+// const demoArray = [1, 2, 3];
 
-const updatedArray = insertAtBeginning(demoArray, -1);
+// const updatedArray = insertAtBeginning(demoArray, -1);
+// updatedArray[0].split(" ");
